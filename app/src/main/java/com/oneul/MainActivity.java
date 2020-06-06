c860ee8940b157c10227077e680a769be4438c4d
Eogeum <Eogeum@naver.com>
2020-06-06 17:55:17 +0900

    Add - Bottom Navigation Bar

--- app/src/main/java/com/oneul/MainActivity.java
+++ app/src/main/java/com/oneul/MainActivity.java
@@ -1,14 +1,61 @@
 package com.oneul;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bot_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        타이틀 바 제거
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e) {
+        }
+
         setContentView(R.layout.activity_main);
+
+//        하단 메뉴 연결 및 선언
+        bot_menu = findViewById(R.id.bot_menu);
+        bot_menu.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
+//        화면 전환 메서드
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.bot_menu_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.bot_menu_write:
+                            openFragment(WriteFragment.newInstance("", ""));
+                            return true;
+                        case R.id.bot_menu_setting:
+                            openFragment(SettingFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
