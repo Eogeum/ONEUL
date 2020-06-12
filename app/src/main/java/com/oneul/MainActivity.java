7c7c525211272a3c0d2ef0eb1fd28eb266e5df55
Eogeum <eogeum@naver.com>
2020-06-12 16:16:58 +0900

    Hide System Navigation & Fix Back Button Event

--- app/src/main/java/com/oneul/MainActivity.java
+++ app/src/main/java/com/oneul/MainActivity.java
@@ -1,12 +1,15 @@
 package com.oneul;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.oneul.fragment.HomeFragment;
@@ -14,8 +17,9 @@ import com.oneul.fragment.SettingFragment;
 import com.oneul.fragment.WriteFragment;
 
 public class MainActivity extends AppCompatActivity {
-    //      변수 선언
+    //      전역변수 선언
     BottomNavigationView bot_menu;
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +30,13 @@ public class MainActivity extends AppCompatActivity {
         bot_menu = findViewById(R.id.bot_menu);
         bot_menu.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         openFragment(HomeFragment.newInstance("", ""));
+
+//        네비게이션 바 숨기기
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
-//        화면 전환 메서드
+    //        화면 전환 메서드
     public void openFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment);
@@ -54,4 +62,26 @@ public class MainActivity extends AppCompatActivity {
                     return false;
                 }
             };
+
+    // 백 버튼 두번 클릭 확인 메서드
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            this.finishAffinity();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "종료하려면 한 번 더 누르세요.", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
+
 }
+
