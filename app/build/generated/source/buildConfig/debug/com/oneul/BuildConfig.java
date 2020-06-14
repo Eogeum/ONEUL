b4d18c202c393306b21ed34dd5b0f4779a5d29e0
Eogeum <eogeum@naver.com>
2020-06-14 15:51:38 +0900

    Add SQLite Datebase

---
+++ app/build/generated/source/buildConfig/debug/com/oneul/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.oneul;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.oneul";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
--- app/src/main/java/com/oneul/fragment/HomeFragment.java
+++ app/src/main/java/com/oneul/fragment/HomeFragment.java
@@ -1,13 +1,23 @@
 package com.oneul.fragment;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.oneul.R;
+import com.oneul.dbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +25,98 @@ import com.oneul.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    //    XML 관련
+    TextView t_oTitle, t_oTime;
+    Button btn_todayBox;
+    EditText et_todayBox;
+    LinearLayout ll_todayBox;
+
+    //    디비 관련
+    dbHelper dbHelper;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View homeView = inflater.inflate(R.layout.fragment_home, container, false);
+
+//        뷰 관련
+        t_oTitle = homeView.findViewById(R.id.t_oTitle);
+        t_oTime = homeView.findViewById(R.id.t_oTime);
+        btn_todayBox = homeView.findViewById(R.id.btn_todayBox);
+        et_todayBox = homeView.findViewById(R.id.et_todayBox);
+        ll_todayBox = homeView.findViewById(R.id.ll_todayBox);
+
+        //        디비 관련
+        dbHelper = new dbHelper(getActivity());
+
+//        날짜 관련
+        Date now = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yy-mm-dd");
+        String showDay = dateFormat.format(now);
+        String today = showDay;
+
+
+//        오늘 날짜가 아닌 경우
+        if (showDay != today) {
+            ll_todayBox.setVisibility(View.GONE);
+        }
+
+        //        todayBox 입력 시
+        btn_todayBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                제목 널값 확인
+                if (TextUtils.isEmpty(et_todayBox.getText().toString())) {
+                    Toast.makeText(getActivity().getApplicationContext(), "일과 제목을 입력해주세요.",
+                            Toast.LENGTH_SHORT).show();
+                    et_todayBox.requestFocus();
+
+                } else {
+
+//                    시간 관련
+                    Date now = new Date(System.currentTimeMillis());
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+                    String startTime = timeFormat.format(now);
+
+//                    일과 값 디비 저장
+                    dbHelper.addOneul(et_todayBox.getText().toString(), startTime, startTime);
+
+//                    일과 값 텍스트 뷰에 설정
+                    String[] result = dbHelper.getOneul(1);
+//
+                    t_oTitle.setText(result[0]);
+                    t_oTime.setText(result[1] + " ~ " + result[2]);
+                }
+
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return homeView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+    //    나중에 점검 해야할 코드들 ####################################
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -45,19 +139,5 @@ public class HomeFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 }
