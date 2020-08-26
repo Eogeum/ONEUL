9885b8e923187145e116ce60cab7e249bc892f8e
hes <es001206@naver.com>
2020-08-26 16:24:25 +0900

    stat fragment

--- app/src/main/java/com/oneul/fragment/StatFragment.java
+++ app/src/main/java/com/oneul/fragment/StatFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.oneul.R;
 
 public class StatFragment extends Fragment {
+
+    Button statLeft, statRight;
+    TextView statMonth;
 
     public StatFragment() {
     }
@@ -22,6 +28,10 @@ public class StatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        인플레이터
         final View statView = inflater.inflate(R.layout.fragment_stat, container, false);
+
+        statLeft = statView.findViewById(R.id.statLeft);
+        statRight = statView.findViewById(R.id.statRight);
+        statMonth = statView.findViewById(R.id.statMonth);
 
         return statView;
     }
