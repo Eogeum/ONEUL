b9688c5f0d642330ba3750eba26fbc8c9d539e4e
hes <es001206@naver.com>
2020-10-08 14:28:39 +0900

    formation basic pie chart shape

--- app/src/main/java/com/oneul/fragment/StatFragment.java
+++ app/src/main/java/com/oneul/fragment/StatFragment.java
@@ -15,11 +15,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.oneul.R;
 import com.oneul.extra.StatAdapter;
 import com.oneul.extra.StatItem;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -46,8 +50,10 @@ public class StatFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        인플레이터
+
+        //인플레이터
         View statView = inflater.inflate(R.layout.fragment_stat, container, false);
+
 
         //년,월 표시
         statLeft = statView.findViewById(R.id.statLeft);
@@ -66,31 +72,30 @@ public class StatFragment extends Fragment {
         statMonth.setText(cuMonth);
 
 
-        //파이차트
+        //파이차트 (그래프형식)
         pieChart = statView.findViewById(R.id.pieChart);
+        ArrayList<PieEntry> pieValue = new ArrayList<>();
 
-        pieChart.setUsePercentValues(true);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.setExtraOffsets(5,10,5,5);
+        pieValue.add(new PieEntry(31f, "코딩"));
+        pieValue.add(new PieEntry(24f, "강의"));
+        pieValue.add(new PieEntry(15f, "여가"));
+        pieValue.add(new PieEntry(11f, "운동"));
+        pieValue.add(new PieEntry(8f, "독서"));
 
-        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        PieDataSet dataSet = new PieDataSet(pieValue, "일과 카테고리");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
 
-        pieChart.setDrawHoleEnabled(false);
-        pieChart.setHoleColor(Color.WHITE);
-        pieChart.setTransparentCircleRadius(61f);
-
-        ArrayList<PieEntry> pValues = new ArrayList<PieEntry>();
-
-        pValues.add(new PieEntry(1, "강의"));
-        pValues.add(new PieEntry(2, "운동"));
-        pValues.add(new PieEntry(3, "여가"));
-        pValues.add(new PieEntry(4, "독서"));
-        pValues.add(new PieEntry(5, "코딩"));
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        pieChart.setData(data);
 
 
-        //일과별 리스트
+        //일과별 리스트 (목록 형식)
         items = new ArrayList<>();
         items.clear();
+
+        //임시 데이터
         items.add(new StatItem("코딩", "31시간"));
         items.add(new StatItem("강의", "24시간"));
         items.add(new StatItem("여가", "15시간"));
